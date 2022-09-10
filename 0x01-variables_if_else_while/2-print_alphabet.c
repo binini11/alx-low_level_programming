0490999057c6cfe3ac127b106abe8c80ffd6b72f
biniam <biniam.y.yohannes@gmail.com>
2022-09-09 19:53:32 -0700

    abc

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*  * main - printsabc in lowercasefirst and the in uppercase.
+*
+*  * Return: Always 0 (Success)
+*/
+int main(void)
+
+{
+        char abc[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (1=0;i<52;i++)
+	{
+		putchar(abc[i])
+	}
+	putchar("\n");
+	return(0)
+
+}

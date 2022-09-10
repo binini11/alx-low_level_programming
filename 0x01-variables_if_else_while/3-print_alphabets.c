f7cfb9ebe4abf69c28c10219e0e180c4cfc83259
biniam <biniam.y.yohannes@gmail.com>
2022-09-09 20:32:00 -0700

    lower and uppercase abc diplay

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*  * main - printsabc in lowercasefirst and the in uppercase.
+*
+*  * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char abc[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(abc[i]);
+	}
+	putchar('\n');
+	return (0);
+}

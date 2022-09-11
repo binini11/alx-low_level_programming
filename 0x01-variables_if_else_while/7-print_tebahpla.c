e9c5ab473ab6b2243c10b65b3fcf334bd691c98a
biniam <biniam.y.yohannes@gmail.com>
2022-09-11 11:07:48 -0700

    reverse alphabet display

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*  * main - printsabc in lowercasefirst and the in uppercase.
+*
+*  * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char abc[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 26; i >= 0; i--)
+	{
+		putchar(abc[i]);
+	}
+	putchar('\n');
+	return (0);
+}

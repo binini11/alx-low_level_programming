e668744632665488b1c7a883b4b04a1a72660bbc
biniam <biniam.y.yohannes@gmail.com>
2022-09-13 12:04:05 -0700

    putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <>
+/**
+ * main - entry pint use _putchar to display
+ *
+ *
+ * Return -  0 success
+ *
+ */
+
+int main(void)
+{
+	char c = "_putchar\n";
+
+	_putchar(c);
+	retutn(0);
+}

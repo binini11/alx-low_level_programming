cf2bb7c7e6124101830e98530fc6334606d45fa2
biniam <biniam.y.yohannes@gmail.com>
2022-09-14 11:57:31 -0700

    lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
 /**
-*  * main - prints als the alphabet in lowercaseg.
+*  * print_alphabet - prints als the alphabet in lowercaseg.
 *
 *  * Return: Always 0 (Success)
 */
-int main(void)
+int print_alphabet(void)
 {
-	print_alphabet();
+	int i
+
+	for (i = 97; i < 123; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,12 +3,5 @@
 
 int _putchar(char);
 void print_alphabet(void)
-{                                                    
-	int i;                                                                                      
-        for (i = 97; i < 123; i++)                          
-        {                                                   
-        _putchar(i);                                        
-	}                                                   
-	_putchar('\n');
-} 
+
 #endif

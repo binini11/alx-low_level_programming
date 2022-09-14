bb4404746427869bf0045c3650eaa1cf2bfeea63
biniam <biniam.y.yohannes@gmail.com>
2022-09-14 16:24:15 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*  *add - checks the alphabet or not.
+*  * @a: in an character
+*  * @b: ins an integer
+*  * Return: integer
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}

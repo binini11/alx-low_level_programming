8ca6df3426b74b49b92be1cb33c90e7d83b4e3a0
biniam <biniam.y.yohannes@gmail.com>
2022-09-14 14:07:58 -0700

    losercase 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*  * print_alphabet - prints als the alphabet in lowercasein a newline 10x.
+*  * print_alphabet_x10 - prints als the alphabet in lowercasein a newline 10x.
 *
 *  * Return: Always 0 (Success)
 */
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 	int i, j;
 	char abc[26] = "abcdefghijklmnopqrstuvwxyz";
 
-	for (i = 0; i < 26; i++)
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 0; j < 10; j++)
+		for (i = 0; i < 10; i++)
 		{
 			_putchar(abc[i]);
 		}

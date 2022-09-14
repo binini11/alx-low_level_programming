ba45f9382d3eb540e380d145946bcfe87e77273b
biniam <biniam.y.yohannes@gmail.com>
2022-09-14 15:17:29 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*  * _abs- computes the absoute value of a number.
+*  *@i: is an integer
+*
+*  * Return: Always 1 ,0
+*/
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}

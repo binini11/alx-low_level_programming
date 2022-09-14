66cde545f8cf21dee14baf5a18613391afadca6c
biniam <biniam.y.yohannes@gmail.com>
2022-09-14 14:46:18 -0700

    alphabet or not

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*  * _alpha - checks the alphabet or not.
+*  * _isalpha - checks the alphabet or not.
 *  * @c: in an character
 *  * Return: Always 1 for lower and 0 if not
 */

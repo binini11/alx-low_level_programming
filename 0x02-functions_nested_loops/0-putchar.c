feed0db7ea96ac6c4a1e107fbf695aa12be41eb6
biniam <biniam.y.yohannes@gmail.com>
2022-09-14 11:02:59 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
-*  * main - number is positive, negative or zero.
+*  * main - prints the _putchar string.
 *
 *  * Return: Always 0 (Success)
 */
 int main(void)
 {
-	int i, character[8] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i, _putchar[8] = {95, 112, 117, 116, 99, 104, 97, 114};
 
 	for (i = 0; i < 8; i++)
 	{
-	putchar(character[i]);
+	putchar(_putchar[i]);
 	}
 	putchar('\n');
 	return (0);

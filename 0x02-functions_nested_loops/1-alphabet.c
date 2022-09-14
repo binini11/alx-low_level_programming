77a0681f1cdd4ee1a31d3d971a8d6ea64ef65bb2
biniam <biniam.y.yohannes@gmail.com>
2022-09-14 11:24:59 -0700

    alphabet in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,8 @@
 *
 *  * Return: Always 0 (Success)
 */
-void print_alphabet(void)
+int main(void)
 {
-	int i;
-
-	for (i = 97; i < 123; i++)
-	{
-	_putchar(i);
-	}
-	_putchar('\n');
+	print_alphabet();
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,14 @@
 #define _main_h
 
 int _putchar(char);
-void print_alphabet(void);
-
+void print_alphabet(void)
+{                                                    
+	int i;                                                                                      
+        for (i = 97; i < 123; i++)                          
+        {                                                   
+        _putchar(i);                                        
+	}                                                   
+	_putchar('\n');                                     
+	return (0);                                         
+} 
 #endif

f404cd01fc9d3c48ab5c7546b40cbe8f4074f5e8
biniam <biniam.y.yohannes@gmail.com>
2022-09-14 11:13:33 -0700

    alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*  * main - prints als the alphabet in lowercaseg.
+*
+*  * Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+	_putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

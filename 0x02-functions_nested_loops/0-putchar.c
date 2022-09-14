73015ab28db79ff482622694b37fe74294a93773
biniam <biniam.y.yohannes@gmail.com>
2022-09-14 10:44:47 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
-#include <>
-/**
- * main - entry pint use _putchar to display
- *
- *
- * Return -  0 success
- *
- */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+/**
+*  * main - number is positive, negative or zero.
+*
+*  * Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char c = "_putchar\n";
+	int i, character[] = {45, 112, 117, 116, 99, 104, 97, 114};
 
-	_putchar(c);
-	retutn(0);
+	for (i = 0; i < sizeof(character); i++)
+	{
+	putchar();
+	}
+	putchar('\n');
+	return (0);
 }

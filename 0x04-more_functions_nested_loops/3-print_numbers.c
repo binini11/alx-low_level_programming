8f2d74333107e6148c62dd289acf2092e6666ba5
biniam <biniam.y.yohannes@gmail.com>
2022-09-16 13:46:48 -0700

    print whole numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - prints fprm 0 - 9.
+* Return: nothing.
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}

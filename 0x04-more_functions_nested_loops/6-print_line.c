d4d0aa3bf05af7cdcce38d87d4aa5949c76a83cd
biniam <biniam.y.yohannes@gmail.com>
2022-09-16 14:51:50 -0700

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_line - print straight line
+* @n: how many times straight line printed
+* Return:noting
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar("\n");
+}

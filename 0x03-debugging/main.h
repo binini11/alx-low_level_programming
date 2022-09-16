eec6141b9e03e69b2f5e9634d45a82d0eaa56636
biniam <biniam.y.yohannes@gmail.com>
2022-09-16 12:22:35 -0700

    ptint out the days in a year including the leap year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day)
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);

1be002c262772f61a31522c11ad861c3668f6273
biniam <biniam.y.yohannes@gmail.com>
2022-09-16 12:27:12 -0700

    days in a yearz

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,8 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-int convert_day(int month, int day)
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);

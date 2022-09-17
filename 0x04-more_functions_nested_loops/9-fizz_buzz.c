d535bf47e316e438633893e6a80460618997f9a5
biniam <biniam.y.yohannes@gmail.com>
2022-09-17 00:07:35 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,15 @@ int main(void)
 	printf("%d", i);
 	for (i = 2; i <= 100; i++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf(" FizzBuzz");
 		}
-		else if (n % 3 == 0)
+		else if (i % 3 == 0)
 		{
 			printf(" Fizz");
 		}
-		else if (n % 5 == 0)
+		else if (i % 5 == 0)
 		{
 			printf(" Buzz");
 		}

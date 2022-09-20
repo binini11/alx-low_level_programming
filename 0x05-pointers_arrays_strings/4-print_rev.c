0e0500e4c55da9a261080170c51f7f59264f3412
biniam <biniam.y.yohannes@gmail.com>
2022-09-20 13:16:00 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_rev - prints a string foolowed b a new line
+* @s: input.
+* Return: 0 success
+*/
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (count >= '\0')
+	{
+		if (s[count] == '\0')
+			_putchar('\n');
+		break;
+		for (count--; count >= 0; count--)
+			_putchar(str[count]);
+		count++;
+	}
+}

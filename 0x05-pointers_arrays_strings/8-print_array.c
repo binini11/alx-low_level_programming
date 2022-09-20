3000c237e8b1edf8ec078e0d86bb8cd5429c7ad1
biniam <biniam.y.yohannes@gmail.com>
2022-09-20 14:15:32 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_array - prints a string line by line
+*@a: input.
+*@n: input.
+*Return: 0 success
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + 1));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}

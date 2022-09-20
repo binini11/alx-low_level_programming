afc2812bf67dfabdf3de8f9a44db888234ed3143
biniam <biniam.y.yohannes@gmail.com>
2022-09-20 12:55:21 -0700

    count

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_strlen - count string length
+* @s: input.
+* Return: count
+* */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	return (count);
+}

21b5566a946de02ee4b5efd0e7bbdafe6734f0f9
biniam <biniam.y.yohannes@gmail.com>
2022-09-20 13:55:57 -0700

    line by line

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*puts2 - prints a string line by line
+*@str: input.
+*Return: 0 success
+*/
+
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}

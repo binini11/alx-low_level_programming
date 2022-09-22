675b239a1ea80339532c64d18afae280d6b3be82
biniam <biniam.y.yohannes@gmail.com>
2022-09-22 14:50:02 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strcmp - takes in two strings and compares them a string and copies it
+* @s2: input.
+* @s1: input.
+*
+*
+* Return: copied string dest
+**/
+
+int _strcmp(char *s1, char *s2)
+{
+	int comparer, i;
+
+	while (compare == 0)
+	{
+	if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+	{
+		break;
+	}
+		compare  = *(s1 + i) - *(s2 + i);
+
+	}
+	return (compare);
+}

f6d7e79def1dc7fba2fb86fbc8565c1ace9c3ab1
biniam <biniam.y.yohannes@gmail.com>
2022-09-25 09:08:39 -0700

    concatinate by n times

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+**_strncat - a concatinates a string.
+* @dest: input.
+* @src: input.
+* @n: input
+*
+* Return: concatinated string dest
+**/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	while (j < n)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\n')
+		{
+			break;
+		}
+		i++;
+		j++
+	}
+	return (dest);
+}

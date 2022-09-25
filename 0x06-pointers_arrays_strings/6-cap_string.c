cc57955122e2028e57ddb417d2ae8962b0ec8fc9
biniam <biniam.y.yohannes@gmail.com>
2022-09-25 12:16:55 -0700

    capitalize every word of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,37 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*string_toupper -changes lower case into uppercase
+*cap_string - changes all words of a string in to capital letter.
 * @s: input.
 *
-* Return: uppercase string s
+* Return: pointer.
 */
 
-char *string_toupper(char *s)
+char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+
+	int separater = {125, 123, 41, 40, 46, 34, 33, 44, 59, 63, 10, 9, 32};
 
 	i = 0;
 
+	if (*(s + i) >= 97 && *(s + i) <= 122)
+	{
+		*(s + i) = *(s - j) - 32;
+	}
+	i++;
 	while (*(s + i) != '\0')
 	{
-		if ((*(s + i) >= 97) && (*(s + i) <= 122))
+		for (j = 0; j < 13; j++)
 		{
-			*(s + i) = *(s + i) - 32;
+			if (*(s + i) == separater[j])
+			{
+				if ((*(s + (i + 1)) >= 97) && (*(s + (i + 1)) <= 122))
+				{
+					*(s + (i + 1)) = *(s + (i + 1)) - 32;
+				}
+				break;
+			}
 		}
 		i++;
 	}

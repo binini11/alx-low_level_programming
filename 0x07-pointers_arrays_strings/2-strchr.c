6da63f1dccedf8476ee47baef5f7f050c8ce7ee1
biniam <biniam.y.yohannes@gmail.com>
2022-09-27 12:32:21 -0700

    search for a character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+**_strchr -  search for a character in a string.
+* @s: input.
+* @c: input.
+* Return: a pointer to a character to a string if found.
+**/
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
+}

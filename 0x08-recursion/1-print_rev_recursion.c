c31066f1cccf961e6e8e4e02d6fb925fd9cfa8ea
biniam <biniam.y.yohannes@gmail.com>
2022-09-28 11:19:14 -0700

    print a string in reversesss

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
-**_puts_recursion -  prints a string in reverse
+*_print_rev_recursion -  prints a string in reverse
 * @s: input.
 * Return: 0 success.
 **/
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		 _puts_recursion(s + 1);
-		 _putchar(*s);
+		_puts_recursion(s + 1);
+		_putchar(*s);
 	}
 	else
+	{
 		_putchar('\n');
+	}
 }

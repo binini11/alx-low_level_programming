07e908e0bbcc406333827a7840dec0cd86d067e7
biniam <biniam.y.yohannes@gmail.com>
2022-09-30 23:41:39 -0700

    multiplicationss

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
 **main - prints multiplication of the input
-* @argc: input
+* @argc: input.
 * @argv: input.
-* Return: 1 on error and o on success
+* Return: 1 on error and 0 on success
 **/
 
 int main(int argc, char *argv[])
@@ -14,6 +15,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }

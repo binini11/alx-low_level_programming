f44b98a0bfdc123ab7adae87cc1ddf87ae67a32c
biniam <biniam.y.yohannes@gmail.com>
2022-09-30 22:36:30 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+**main - prints nnumber of arguments passed
+* @argc: input
+* @argv: input.
+* Return: 0 on success.
+**/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

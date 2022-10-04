5678d226c9173eeb78d1361d89b4223fc83aebb7
biniam <biniam.y.yohannes@gmail.com>
2022-10-04 13:18:53 -0700

    pinter to memory allovated

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns apinter to anewly allocated memory space
+ * @str: input
+ * Return: a pinter
+**/
+
+char *_strdup(char *str);
+{
+	unsigned int i, j;
+	char *b;
+
+	if (str == 0)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	b = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (b == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		b[i] = str[j];
+
+	return (b);
+}

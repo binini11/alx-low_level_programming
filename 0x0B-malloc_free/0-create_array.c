5dc86f7f56d8f70c1c7c21e08e2d8ddf3ce5f653
biniam <biniam.y.yohannes@gmail.com>
2022-10-04 12:59:26 -0700

    arry of charactersz

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,10 +15,14 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
+
 	b = malloc(sizeof(c) * size);
+
 	if (b == NULL)
 		return (NULL);
+
 	for (i = 0; i < size; i++)
 		b[i] = c;
+
 	return (b);
 }

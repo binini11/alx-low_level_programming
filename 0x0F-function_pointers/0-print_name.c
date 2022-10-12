3bdb3fecfc877df3a85e9f904eda94acf422e972
biniam <biniam.y.yohannes@gmail.com>
2022-10-12 09:39:56 -0700

    print my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: input name.
+ * @ptr: function pointer.
+ * Return: no return.
+ */
+
+void print_name(char *name, void (*ptr)(char *))
+{
+	if (name && ptr)
+		ptr(name);
+
+}

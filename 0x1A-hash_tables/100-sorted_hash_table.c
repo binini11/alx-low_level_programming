7f600ba77f44fe5f5714cd2bc3e0eef4ea0f0899
biniam <biniam.y.yohannes@gmail.com>
2023-02-02 22:29:21 -0800

    has

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,7 +1,5 @@
 #include "hash_tables.h"
 
-
-
 /**
  * shash_table_create - creates a shash table with a given size
  *
@@ -11,52 +9,25 @@
 
 shash_table_t *shash_table_create(unsigned long int size)
 {
-
 	shash_table_t *table;
-
 	shash_node_t **array;
-
 	unsigned long int i;
-
-
 
 	table = malloc(sizeof(shash_table_t));
 
 	if (table == NULL)
-
 		return (NULL);
-
-
-
 	array = malloc(sizeof(shash_node_t *) * size);
-
 	if (array == NULL)
-
 		return (NULL);
-
-
-
 	for (i = 0; i < size; i++)
-
 		array[i] = NULL;
-
-
-
 	table->array = array;
-
 	table->size = size;
-
 	table->shead = NULL;
-
 	table->stail = NULL;
-
-
-
 	return (table);
-
 }
-
-
 
 /**
  * add_n_shash - adds a node at the beginning of a shash at a given index
@@ -69,64 +40,31 @@ shash_table_t *shash_table_create(unsigned long int size)
 
 shash_node_t *add_n_shash(shash_node_t **h, const char *key, const char *value)
 {
-
 	shash_node_t *tmp;
-
-
-
 	tmp = *h;
 
-
-
 	while (tmp != NULL)
-
 	{
-
 		if (strcmp(key, tmp->key) == 0)
-
 		{
-
 			free(tmp->value);
-
 			tmp->value = strdup(value);
-
 			return (tmp);
-
 		}
-
 		tmp = tmp->next;
-
 	}
-
-
 
 	tmp = malloc(sizeof(shash_node_t));
 
-
-
 	if (tmp == NULL)
-
 		return (NULL);
 
-
-
 	tmp->key = strdup(key);
-
 	tmp->value = strdup(value);
-
 	tmp->next = *h;
-
-
-
 	*h = tmp;
-
-
-
 	return (tmp);
-
 }
-
-
 
 /**
  * add_i_shash - adds a node on the DLL of the shash table
@@ -138,88 +76,39 @@ shash_node_t *add_n_shash(shash_node_t **h, const char *key, const char *value)
 
 void add_i_shash(shash_table_t *ht, shash_node_t *new)
 {
-
 	shash_node_t *tmp1, *tmp2;
-
 	int ret;
-
-
-
 	tmp1 = tmp2 = ht->shead;
 
-
-
 	while (tmp1 != NULL)
-
 	{
-
 		ret = strcmp(new->key, tmp1->key);
-
 		if (ret == 0)
-
 		{
-
 			return;
-
 		}
-
 		else if (ret < 0)
-
 		{
-
 			new->sprev = tmp1->sprev;
-
-
-
 			if (tmp1->sprev)
-
 				tmp1->sprev->snext = new;
-
 			else
-
 				ht->shead = new;
-
-
-
 			tmp1->sprev = new;
-
 			new->snext = tmp1;
-
-
-
 			return;
-
 		}
-
 		tmp2 = tmp1;
-
 		tmp1 = tmp1->snext;
-
 	}
-
-
-
 	new->sprev = tmp2;
-
 	new->snext = NULL;
-
-
-
 	if (ht->shead)
-
 		tmp2->snext = new;
-
 	else
-
 		ht->shead = new;
-
-
-
 	ht->stail = new;
-
 }
-
-
 
 /**
  * shash_table_set - adds a hash (key, value) to a given shash table
@@ -234,46 +123,19 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
 
 	unsigned long int k_index;
-
 	shash_node_t *new;
-
-
-
 	if (ht == NULL)
-
 		return (0);
-
-
 
 	if (key == NULL || *key == '\0')
-
 		return (0);
-
-
-
 	k_index = key_index((unsigned char *)key, ht->size);
-
-
-
 	new = add_n_shash(&(ht->array[k_index]), key, value);
-
-
-
 	if (new == NULL)
-
 		return (0);
-
-
-
 	add_i_shash(ht, new);
-
-
-
 	return (1);
-
 }
-
-
 
 /**
  * shash_table_get - retrieves a value associated with a key
@@ -285,52 +147,24 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 
 char *shash_table_get(const shash_table_t *ht, const char *key)
 {
-
 	unsigned long int k_index;
-
 	shash_node_t *tmp;
-
-
-
 	if (ht == NULL)
-
 		return (NULL);
-
-
 
 	if (key == NULL || *key == '\0')
-
 		return (NULL);
 
-
-
 	k_index = key_index((unsigned char *)key, ht->size);
-
-
-
 	tmp = ht->array[k_index];
-
-
-
 	while (tmp != NULL)
-
 	{
-
 		if (strcmp(tmp->key, key) == 0)
-
 			return (tmp->value);
-
 		tmp = tmp->next;
-
 	}
-
-
-
 	return (NULL);
-
 }
-
-
 
 /**
  * shash_table_print - prints the keys and values of the shash table
@@ -341,48 +175,23 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 
 void shash_table_print(const shash_table_t *ht)
 {
-
 	shash_node_t *tmp;
-
 	char *sep;
 
-
-
 	if (ht == NULL)
-
 		return;
 
-
-
 	printf("{");
-
 	sep = "";
-
-
-
 	tmp = ht->shead;
-
-
-
 	while (tmp != NULL)
-
 	{
-
 		printf("%s'%s': '%s'", sep, tmp->key, tmp->value);
-
 		sep = ", ";
-
 		tmp = tmp->snext;
-
 	}
-
-
-
 	printf("}\n");
-
 }
-
-
 
 /**
  * shash_table_print_rev - prints the keys and values of the shash table
@@ -394,15 +203,10 @@ void shash_table_print(const shash_table_t *ht)
 
 void shash_table_print_rev(const shash_table_t *ht)
 {
-
 	shash_node_t *tmp;
-
 	char *sep;
 
-
-
 	if (ht == NULL)
-
 		return;
 
 

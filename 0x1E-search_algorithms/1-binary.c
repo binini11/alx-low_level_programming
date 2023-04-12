227e1c2401b03f2902e291a4afaf3387c3586fab
biniam <biniam.y.yohannes@gmail.com>
2023-04-12 13:25:44 -0700

    binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,58 +1,37 @@
-i#include "search_algos.h"
+#include "search_algos.h"
+
 
 /**
- * recursive_search - searches for a value in an array of
+ * binary_search - searches for a value in a sorted arrayay of
  * integers using the Binary search algorithm
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
-int recursive_search(int *array, size_t size, int value)
-{
-	size_t half = size / 2;
-	size_t i;
-
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-		return ((int)half);
-
-	if (value < array[half])
-		return (recursive_search(array, half, value));
-
-	half++;
-
-	return (recursive_search(array + half, size - half, value) + half);
-}
-
-/**
- * binary_search - calls to binary_search to return
- * the index of the number
-  * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: first index where value is located or -1 otherwise
+*/
 int binary_search(int *array, size_t size, int value)
 {
-	int index;
+	int start = 0, end, mid, i;
 
-	index = recursive_search(array, size, value);
+	end = size - 1;
+	while (start <= end)
+	{
+		printf("Searching in array: ");
 
-	if (index >= 0 && array[index] != value)
-		return (-1);
+		for (i = start; i <= end; i++)
+			i < end ? printf("%d, ", array[i]) : printf("%d\n", array[i]);
 
-	return (index);
+		mid = (start + end) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+
+		else if (value < array[mid])
+			end = mid - 1;
+
+		else
+			start = mid + 1;
+	}
+	return (-1);
 }
+
